4c921c1d8aafbc77a82aace3a5d41c6c7f443e45
safiulalam99 <safiulalam99>
2022-07-15 02:30:39 +0300

    site status update

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-const sites = require("./sites.json");
 const fs = require("fs");
 const request = require("request");
-const { url } = require("inspector");
 let logContent = {};
 let dataToLog = [];
 let checkedURL;
 
+const statusCode = {
+    'code':'404 Informational Request',
+    'code':'2 xx Successful Request',
+    'code':'3 xx Redirects',
+    'code':'4 xx Client Errors',
+    'code':'5 xx Server Errors',
+} 
+console.log(statusCode)
+
+// reads the URL and content of the URL to compare if they exist within the body 
+// of the rquest page
 const readFromFile = async () => {
   fs.readFile("./sites.json", "utf-8", async (err, jsonData) => {
     if (err) {
@@ -27,16 +36,21 @@ const UrlCheck = async (URL) => {
     await request(
       { url: URL.url, time: true },
       function (error, response, body) {
+        var time = new Date();
+        // var initialResponseTime = 
+    
         logContent = {
-          name: URL.url,
+            URL: URL.url,
+            last_checked: time,
           responseTime: !response ? "No response" : response.elapsedTime,
-          statusCode: !response ? errorCode : response.statusCode,
-          body: [!response ? "No response" : body],
-          content_match: response
-            ? body.includes(URL.content)
-              ? true
-              : false
-            : "No response",
+          statusCode: !response ? 'failed' : response.statusCode,
+            // body: [!response ? "No response" : body],
+            // content_match: response
+            //   ? body.includes(URL.content)
+            //     ? true
+            //     : false
+            //   : "No content found",
+          status:response ? (response.statusCode==undefined ?'down':'up') : 'down',
         };
         dataToLog.push(logContent);
         writingToFile(dataToLog);
@@ -46,8 +60,10 @@ const UrlCheck = async (URL) => {
     console.log("#####" + err);
   }
 };
+
+//function to write the json object to the requestList.json file
 const writingToFile = (logFile) => {
-  fs.writeFile("./finalList.json", JSON.stringify(logFile, null, 2), (err) => {
+  fs.writeFile("./requestList.json", JSON.stringify(logFile, null, 2), (err) => {
     if (err) {
       console.log(err);
     } else {
